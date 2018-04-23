9a3b056dccf7de8f416e804289857b95125671fa
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/CascadedLookup/index.js
@@ -0,0 +1,16 @@
+﻿window.CascadedLookup = window.CascadedLookup || {};
+
+$(function() {
+    // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
+    // DevExpress.devices.current({ platform: "generic" });
+
+    CascadedLookup.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: CascadedLookup,        
+        layoutSet: DevExpress.framework.html.layoutSets[CascadedLookup.config.layoutSet],
+        navigation: CascadedLookup.config.navigation,
+        commandMapping: CascadedLookup.config.commandMapping
+    });
+    CascadedLookup.app.router.register(":view/:id", { view: "home", id: undefined });
+
+    CascadedLookup.app.navigate();
+});
