7589d18684e856ae678958b1d07e427e0685f33d
Tom <meschken@yandex.ru>
2025-07-28 22:27:21 +0400

    fix Vue

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,28 +1,107 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import DxButton from 'devextreme-vue/button';
+import 'devextreme/dist/css/dx.light.css';
 
-const props = defineProps({
-  text: {
-    type: String,
-    default: 'count',
+import { DxSelectBox, type DxSelectBoxTypes } from 'devextreme-vue/select-box';
+import DxForm, { DxSimpleItem, DxLabel, type DxFormTypes } from 'devextreme-vue/form';
+import type dxSelectBox from 'devextreme/ui/select_box';
+import notify from 'devextreme/ui/notify';
+
+import service from '../data';
+
+const states = service.getStates(),
+  cities = service.getCities(),
+  address = ref(service.getAddress());
+const citySelectBoxRef = ref<DxSelectBox>();
+const cityValue = ref(null);
+const stateEditorOptions = {
+    dataSource: states,
+    valueExpr: 'ID',
+    displayExpr: 'Name'
   },
-});
-const count = ref(0);
-const buttonText = computed<string>(
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
+  cityEditorOptions = {
+    dataSource: cities,
+    valueExpr: 'ID',
+    displayExpr: 'Name'
+  };
+function stateValueChanged(e: DxSelectBoxTypes.ValueChangedEvent) {
+  const citySelectBox = citySelectBoxRef?.value?.instance;
+  if (citySelectBox) {
+    const dataSource = citySelectBox.getDataSource();
+    dataSource.filter(['StateID', '=', e.value]);
+    dataSource.load().then(() => {
+      cityValue.value = null;
+    }).catch(() => {
+      notify('An error occurred while loading changes.', 'error', 3000);
+    });
+  }
+}
+function fieldDataChanged(e: DxFormTypes.FieldDataChangedEvent) {
+  if (e.dataField === 'StateID') {
+    const cityEditor = e.component.getEditor('CityID') as dxSelectBox;
+    const dataSource = cityEditor.getDataSource();
+    dataSource.filter(['StateID', '=', e.value]);
+    dataSource.load().then(() => {
+      address.value.CityID = null;
+    }).catch(() => {
+      notify('An error occurred while loading changes.', 'error', 3000);
+    });
+  }
 }
 </script>
 <template>
   <div>
-    <DxButton
-      :text="buttonText"
-      @click="clickHandler"
-    />
+    <div
+      class="dx-fieldset"
+      style="width:50%"
+    >
+      <div class="dx-fieldset-header">Select State, City</div>
+      <div class="dx-field">
+        <div class="dx-field-label">State</div>
+        <div class="dx-field-value">
+          <DxSelectBox
+            :data-source="states"
+            value-expr="ID"
+            display-expr="Name"
+            @valueChanged="stateValueChanged"
+          />
+        </div>
+      </div>
+      <div class="dx-field">
+        <div class="dx-field-label">City</div>
+        <div class="dx-field-value">
+          <DxSelectBox
+            ref="citySelectBoxRef"
+            :data-source="cities"
+            v-model:value="cityValue"
+            value-expr="ID"
+            display-expr="Name"
+          />
+        </div>
+      </div>
+      <div class="dx-fieldset-header">In Form</div>
+      <div class="dx-field">
+        <DxForm
+          v-model:form-data="address"
+          @field-data-changed="fieldDataChanged"
+        >
+          <DxSimpleItem
+            data-field="StateID"
+            editor-type="dxSelectBox"
+            :editor-options="stateEditorOptions"
+          >
+            <DxLabel text="State"/>
+          </DxSimpleItem>
+          <DxSimpleItem
+            data-field="CityID"
+            editor-type="dxSelectBox"
+            :editor-options="cityEditorOptions"
+          >
+            <DxLabel text="City"/>
+          </DxSimpleItem>
+        </DxForm>
+      </div>
+    </div>
   </div>
 </template>
---
+++ Vue/src/data.ts
@@ -0,0 +1,155 @@
+export interface State {
+  ID: number;
+  Name: string;
+}
+
+export interface City {
+  ID: number;
+  Name: string;
+  StateID: number | null;
+}
+
+export interface Address {
+  StateID: number | null;
+  CityID: number | null;
+}
+
+const address: Address = { StateID: null, CityID: null };
+
+const states: State[] = [
+  {
+    ID: 1,
+    Name: 'Alabama',
+  },
+  {
+    ID: 2,
+    Name: 'Alaska',
+  },
+  {
+    ID: 3,
+    Name: 'Arizona',
+  },
+  {
+    ID: 4,
+    Name: 'Arkansas',
+  },
+  {
+    ID: 5,
+    Name: 'California',
+  },
+];
+
+const cities: City[] = [
+  {
+    ID: 1,
+    Name: 'Tuscaloosa',
+    StateID: 1,
+  },
+  {
+    ID: 2,
+    Name: 'Hoover',
+    StateID: 1,
+  },
+  {
+    ID: 3,
+    Name: 'Dothan',
+    StateID: 1,
+  },
+  {
+    ID: 4,
+    Name: 'Decatur',
+    StateID: 1,
+  },
+  {
+    ID: 5,
+    Name: 'Anchorage',
+    StateID: 2,
+  },
+  {
+    ID: 6,
+    Name: 'Fairbanks',
+    StateID: 2,
+  },
+  {
+    ID: 7,
+    Name: 'Juneau',
+    StateID: 2,
+  },
+  {
+    ID: 8,
+    Name: 'Avondale',
+    StateID: 3,
+  },
+  {
+    ID: 9,
+    Name: 'Buckeye',
+    StateID: 3,
+  },
+  {
+    ID: 10,
+    Name: 'Carefree',
+    StateID: 3,
+  },
+  {
+    ID: 11,
+    Name: 'Springdale',
+    StateID: 4,
+  },
+  {
+    ID: 12,
+    Name: 'Rogers',
+    StateID: 4,
+  },
+  {
+    ID: 13,
+    Name: 'Sherwood',
+    StateID: 4,
+  },
+  {
+    ID: 14,
+    Name: 'Jacksonville',
+    StateID: 4,
+  },
+  {
+    ID: 15,
+    Name: 'Cabot',
+    StateID: 4,
+  },
+  {
+    ID: 16,
+    Name: 'Adelanto',
+    StateID: 5,
+  },
+  {
+    ID: 17,
+    Name: 'Glendale',
+    StateID: 5,
+  },
+  {
+    ID: 18,
+    Name: 'Moorpark',
+    StateID: 5,
+  },
+  {
+    ID: 19,
+    Name: 'Needles',
+    StateID: 5,
+  },
+  {
+    ID: 20,
+    Name: 'Ontario',
+    StateID: 5,
+  },
+];
+
+export default {
+  getStates(): State[] {
+    return states;
+  },
+  getCities(): City[] {
+    return cities;
+  },
+  getAddress(): Address {
+    return address;
+  },
+};
