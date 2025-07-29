08fe338a79e896cc6a64eb0ed8ea112a47e0ff31
Tom <meschken@yandex.ru>
2025-07-29 10:59:26 +0400

    add new project

---
+++ ASP.NET Core/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Core.Models
+{
+    public class Address
+    {
+        public int? StateID { get; set; }
+        public int? CityID { get; set; }
+
+        public static readonly Address Empty = new Address();
+    }
+}
